f931749a3592c317b70a36f59655bd7b4647351c
Levi9 <levi9@Levi9s-Mac-mini.levi9.com>
2019-02-14 13:41:00 +0100

    Initial commit

---
+++ src/android/NotchPlugin.java
@@ -0,0 +1,29 @@
+package cordova-plugin-notch;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class NotchPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("has")) {
+            this.has(callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void has(CallbackContext callbackContext) {
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        callbackContext.success(resourceId > 0);
+    }
+}
---
+++ src/ios/NotchPlugin.m
@@ -0,0 +1,32 @@
+/********* NotchPlugin.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface NotchPlugin : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)has:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation NotchPlugin
+
+- (void)has:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    
+    bool hasNotch;
+
+    hasNotch = true;
+
+    if (@available(iOS 11.0, *)) {
+        UIWindow *window = UIApplication.sharedApplication.keyWindow;
+        hasNotch = window.safeAreaInsets.bottom > 0;
+    }
+
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:hasNotch];
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end

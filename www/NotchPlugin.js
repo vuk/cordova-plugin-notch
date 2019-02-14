89627d1aecbd2bba37a113b8982d284ce8738b9e
Vuk Stankovic <vuks89@gmail.com>
2019-02-14 13:43:53 +0100

    Update js file

--- www/NotchPlugin.js
+++ www/NotchPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'NotchPlugin', 'coolMethod', [arg0]);
+exports.has = function (arg0, success, error) {
+    exec(success, error, 'NotchPlugin', 'has', [arg0]);
 };
